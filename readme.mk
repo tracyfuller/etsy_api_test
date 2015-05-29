5dab2b25c866d771b0804bf9e9b5cc626abc2047
tracyfuller <tracy.e.fuller@gmail.com>
2015-05-29 17:14:47 -0500

    Update readme with documentation links

--- readme.mk
+++ readme.mk
@@ -1,3 +1,17 @@
 #ETSY API TEST
 I am testing the use of the Etsy API to get an appropriate setup for use in my personal project.
 
+masonry documentation:
+http://masonry.desandro.com/
+
+Masonry requires imagesloaded:
+http://masonry.desandro.com/appendix.html#imagesloaded
+
+imagesloaded:
+http://imagesloaded.desandro.com/
+
+masonry doesn't play with angular. To make it play, you need this:
+http://passy.github.io/angular-masonry/
+(warning, doesn't have documentation)
+
+TO DO: Masonry needs to fire /after/ the images are loaded. imagesloaded delays a function while the images are loading.
